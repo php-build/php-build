db4af2bdf48282377db242b8cecaa717415e5e19
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-08-16 18:03:18 +0200

    Added Plugins for xdebug and pyrus installation

---
+++ plugins/pyrus.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+function install_pyrus {
+    echo "Installing Pyrus..."
+
+    local pyrus_url="http://pear2.php.net/pyrus.phar"
+
+    if [ -f "$PREFIX/bin/pyrus.phar" ]; then
+        rm "$PREFIX/bin/pyrus.phar"
+    fi
+
+    wget -qP "$PREFIX/bin" $pyrus_url
+
+    if [ ! -d "$PREFIX/pear" ]; then
+        mkdir "$PREFIX/pear"
+    fi
+
+    local pyrus_home="$PREFIX/share/pear"
+
+    # Create Pyrus' own Home Directory
+    # so it will not place them in the real user's home directory
+    if [ ! -d "$pyrus_home" ]; then
+        mkdir -p "$pyrus_home"
+    fi
+
+    # Add the directory where the PHP Files of PEAR Packages get installed
+    # to PHP's include path
+    echo "include_path=.:$PREFIX/pear/php" > "$PREFIX/etc/conf.d/pear.ini"
+
+    # Create the Pyrus executable
+    #
+    local pyrus_sh="$PREFIX/bin/pyrus"
+
+    echo "#!/bin/bash" > $pyrus_sh
+
+    # Pyrus looks for its config by default in the User's Home Directory,
+    # so define a separate Home Directory just for Pyrus to isolate
+    # the Configs between PHP versions
+    echo "export HOME=$pyrus_home" >> $pyrus_sh
+    echo "$PREFIX/bin/php -dphar.readonly=0 $PREFIX/bin/pyrus.phar \$*" >> $pyrus_sh
+
+    chmod +x "$PREFIX/bin/pyrus"
+
+    # Setup Pyrus to place executables in the version's bin directory
+    # so executables can be later easier collected on rehash
+    local pear_sysconfig=$(cat <<EOF
+<?xml version="1.0"?>
+<pearconfig version="1.0">
+    <bin_dir>$PREFIX/bin</bin_dir>
+</pearconfig>
+EOF
+)
+
+    echo "$pear_sysconfig" > "$PREFIX/pear/.config"
+
+    # Create the default pearconfig.xml by hand, otherwise the
+    # User would be asked for the PEAR path on the first run.
+    local pear_config=$(cat <<EOF
+<?xml version="1.0"?>
+<pearconfig version="1.0">
+    <default_channel>pear2.php.net</default_channel>
+    <auto_discover>0</auto_discover>
+    <http_proxy></http_proxy>
+    <cache_dir>$PREFIX/pear/cache</cache_dir>
+    <temp_dir>$PREFIX/pear/temp</temp_dir>
+    <verbose>1</verbose>
+    <preferred_state>stable</preferred_state>
+    <umask>0022</umask>
+    <cache_ttl>3600</cache_ttl>
+    <my_pear_path>$PREFIX/pear</my_pear_path>
+    <plugins_dir>$PREFIX/share/pear/.pear</plugins_dir>
+</pearconfig>
+EOF
+)
+
+    if [ ! -d "$pyrus_home/.pear" ]; then
+        mkdir "$pyrus_home/.pear"
+    fi
+
+    echo $pear_config > "$pyrus_home/.pear/pearconfig.xml"
+
+    echo Done.
+}
---
+++ plugins/xdebug.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+function install_xdebug_master {
+    local TEMP_DIR="$PHP_BUILD_ROOT/tmp"
+
+    if [ -d "$TEMP_DIR/xdebug" ] && [ -d "$TEMP_DIR/xdebug/.git" ]; then
+        local old_pwd=$(pwd)
+
+        echo "Updating XDebug from master"
+
+        cd "$TEMP_DIR/xdebug"
+        git pull origin master > /dev/null
+        cd "$old_pwd"
+    else
+        echo "Fetching XDebug from master"
+        git clone git://github.com/derickr/xdebug.git "$TEMP_DIR/xdebug" > /dev/null
+    fi
+
+    _build_xdebug "$TEMP_DIR/xdebug"
+
+    git --git-dir="$TEMP_DIR/xdebug/.git" reset --hard HEAD
+}
+
+function install_xdebug {
+    local version=$1
+
+    if [ -z "$version" ]; then
+        return 1
+    fi
+
+    if [ ! -f "$PHP_BUILD_ROOT/tmp/xdebug-$version.tgz" ]; then
+        wget -qP "$PHP_BUILD_ROOT/tmp" "http://xdebug.org/files/xdebug-$version.tgz"
+    fi
+
+    if [ -d "$PHP_BUILD_ROOT/tmp/xdebug-$version" ]; then
+        rm "$PHP_BUILD_ROOT/tmp/xdebug-$version" -rf
+    fi
+
+    tar -xzf "$PHP_BUILD_ROOT/tmp/xdebug-$version.tgz" -C "$PHP_BUILD_ROOT/tmp"
+
+    _build_xdebug "$PHP_BUILD_ROOT/tmp/xdebug-$version"
+}
+
+function _build_xdebug {
+    local source_dir="$1"
+
+    echo "Installing the XDebug Extension..."
+
+    local old_pwd=$(pwd)
+
+    cd "$source_dir"
+
+    local php_version=$(echo "<?php echo PHP_VERSION;" | "$PREFIX/bin/php")
+
+    $PREFIX/bin/phpize > /dev/null
+    "$(pwd)/configure" --enable-xdebug --with-php-config=$PREFIX/bin/php-config > /dev/null
+
+    make > /dev/null
+    make install > /dev/null
+
+    local xdebug_ini="$PREFIX/etc/conf.d/xdebug.ini"
+
+    # Somehow xdebug needs the absolute path to the extensions directory
+    # to get loaded, so get it from the PHP binary
+    local extension_dir=$(echo "<?php echo ini_get('extension_dir');" | "$PREFIX/bin/php")
+
+    if [ ! -f "$xdebug_ini" ]; then
+        echo "Installing xdebug.ini"
+        echo "zend_extension=\"$extension_dir/xdebug.so\"" > $xdebug_ini
+        echo "html_errors=on" >> $xdebug_ini
+    fi
+
+    cd "$old_pwd"
+
+    echo "Done."
+}
