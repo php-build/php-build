716d96dc0cba8d62a5bf9a541c6be7a0b1810290
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-05-07 16:01:35 +0200

    make-release.sh now complains when no version is passed.

--- make-release.sh
+++ make-release.sh
@@ -2,8 +2,12 @@
 
 VERSION="$1"
 
+if [ -z "$VERSION" ]; then
+    echo "make-release.sh: No Version given." >&2
+    exit 1
+fi
+
 ./build-docs.sh "$VERSION"
 
 # Replace {{VERSION}} with the version passed as argument:
-
 sed -e "s/{{VERSION}}/$VERSION/" -i '' "bin/php-build"
