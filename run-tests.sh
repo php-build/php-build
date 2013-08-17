e2e1cac1c1aa941e0b1c60661e60cad7a6b4c84c
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2013-08-17 14:59:19 -0300

    Update STABLE_DEFINITIONS with 5.5.2 - #142
    Signed-off-by: Rogerio Prado de Jesus <rogeriopradoj@gmail.com>

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.27 5.4.17 5.5.1"
+STABLE_DEFINITIONS="5.3.27 5.4.17 5.5.2"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
