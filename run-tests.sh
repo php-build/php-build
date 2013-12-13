741e71dafaa6788ba1ab0459a2a7275dc30edf97
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2013-12-13 11:33:33 +0100

    Update PHP to 5.3.28, 5.4.23, 5.5.7

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.27 5.4.22 5.5.6"
+STABLE_DEFINITIONS="5.3.28 5.4.23 5.5.7"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
