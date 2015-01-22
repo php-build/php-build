2718f0d60dc9bae86fbd0575e3a7202b4f8fbc5e
Graham Campbell <graham@mineuk.com>
2015-01-22 18:33:52 +0000

    Added php 5.4.27, 5.5.21, and 5.6.5

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.36 5.5.20 5.6.4"
+STABLE_DEFINITIONS="5.3.29 5.4.37 5.5.21 5.6.5"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
