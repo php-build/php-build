a041f7e4cf87bbb6b3e405da20ccb39ed50449f4
Gerard Roche <gerardroche@users.noreply.github.com>
2015-04-19 22:26:02 +0100

    Installer log message cleanup and other :lipstick:

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ BIN_DIR="$PREFIX/bin"
 SHARE_DIR="$PREFIX/share"
 MAN_DIR="$PREFIX/share/man"
 
-echo -n "  - Creating Directories..."
+echo -n "  - creating directories..."
 
 [ ! -d "$BIN_DIR" ]          && mkdir -p "$BIN_DIR"
 [ ! -d "$MAN_DIR" ]          && mkdir -p "$MAN_DIR"
@@ -22,12 +22,14 @@ echo -n "  - Creating Directories..."
 [ ! -d "$MAN_DIR/man1" ]     && mkdir -p "$MAN_DIR/man1"
 [ ! -d "$MAN_DIR/man5" ]     && mkdir -p "$MAN_DIR/man5"
 
-echo " Done."
+echo " done."
 
-echo -n "  - Copying files..."
-cp -R "$DIR/"{bin,share} "$PREFIX/"
+echo -n "  - copying files..."
 
+cp -r "$DIR/"{bin,share} "$PREFIX/"
 cp "$DIR/man/php-build.1" "$MAN_DIR/man1/"
 cp "$DIR/man/php-build.5" "$MAN_DIR/man5/"
 
-echo " Done."
+echo " done."
+
+echo "Done."
