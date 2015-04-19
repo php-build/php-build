f901aced53a770c69de56e55ea864767645eab30
Gerard Roche <gerardroche@users.noreply.github.com>
2015-04-19 22:22:49 +0100

    Installer doesn't need to create tmp files
    There is no need to create temporary files, the build script creates
    these when needed.

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ echo "Installing php-build in $PREFIX"
 
 BIN_DIR="$PREFIX/bin"
 MAN_DIR="$PREFIX/share/man"
-TMP_DIR="/tmp/php-build"
 
 echo -n "  - Creating Directories..."
 
@@ -20,12 +19,8 @@ echo -n "  - Creating Directories..."
 [ ! -d "$MAN_DIR" ]          && mkdir -p "$MAN_DIR"
 [ ! -d "$MAN_DIR/man1" ]     && mkdir -p "$MAN_DIR/man1"
 [ ! -d "$MAN_DIR/man5" ]     && mkdir -p "$MAN_DIR/man5"
-[ ! -d "$TMP_DIR/packages" ] && mkdir -p "$TMP_DIR/packages"
-[ ! -d "$TMP_DIR/source" ]   && mkdir -p "$TMP_DIR/source"
 
 echo " Done."
-
-chmod -R 0777 "$TMP_DIR"
 
 echo -n "  - Copying files..."
 cp -R "$DIR/"{bin,share} "$PREFIX/"
