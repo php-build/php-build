ef2ec0a3acbc6b5de8bdb574b39a60f513bf7a1f
Graham Campbell <graham@alt-three.com>
2015-11-15 12:14:59 +0000

    Added PHP 7.0.0RC7

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.30 5.6.15 7.0.0RC6"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.30 5.6.15 7.0.0RC7"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
