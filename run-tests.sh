1a033e694a6d2a3ac72af7e9adb022e372e89aa2
Trevor Suarez <rican7@gmail.com>
2015-12-03 15:58:31 -0500

    Updating tests and docs

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.30 5.6.16 7.0.0RC8"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.30 5.6.16 7.0.0"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
