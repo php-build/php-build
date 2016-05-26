f36ea7b13055ced08a9c77ca000685608da827e1
Graham Campbell <graham@alt-three.com>
2016-05-26 22:48:18 +0100

    Added the latest PHP versions

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.35 5.6.21 7.0.6"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.36 5.6.22 7.0.7"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
