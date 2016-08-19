2a450d62a7e09a46413f39bc081cb6b10e96df23
Graham Campbell <graham@bugsnag.com>
2016-08-19 13:27:53 +0100

    Added php 7.1 beta 3 and removed the old stuff
    Closes #395.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.24 7.0.9 7.1.0beta2"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.24 7.0.9 7.1.0beta3"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
