9223d4ef0ce687256128dc666e2cc6e17196e072
moznion <moznion@gmail.com>
2017-02-20 12:05:05 +0900

    Add PHP 7.0.16 definition

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.30 7.0.15 7.1.2"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.30 7.0.16 7.1.2"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
