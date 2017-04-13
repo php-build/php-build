35a58b304d0402711c6d3a949b8ce48fd7b5e7d0
Graham Campbell <graham@alt-three.com>
2017-04-13 23:15:26 +0100

    Added PHP 7.0.18 and 7.1.4

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.30 7.0.17 7.1.3"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.30 7.0.18 7.1.4"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
