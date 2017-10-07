a1c8e433140dd38b075817c203feeb24d3ca8e7a
Graham Campbell <graham@alt-three.com>
2017-10-07 11:23:42 +0100

    Replced PHP 7.2beta3 with 7.2RC3

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.31 7.0.24 7.1.10 7.2.0RC2"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.31 7.0.24 7.1.10 7.2.0RC3"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
