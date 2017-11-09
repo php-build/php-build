5a025ea80190934ebcbae4db096c4ac506a878aa
Paweł Krzaczkowski <pawel@krzaczkowski.pl>
2017-11-09 23:17:07 +0100

    added PHP 5.6.32 (#481)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.31 7.0.25 7.1.11 7.2.0RC5"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.32 7.0.25 7.1.11 7.2.0RC5"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
