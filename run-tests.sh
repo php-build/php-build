ad13c4627e82d65180f4a713aa0118ad954361ac
TAKAHASHI Kunihiko <kunihiko.takahashi@gmail.com>
2019-01-10 10:24:06 +0900

    Added PHP 7.1.26 7.2.14 7.3.1

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.39 7.0.33 7.1.25 7.2.13 7.3.0"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.39 7.0.33 7.1.26 7.2.14 7.3.1"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
