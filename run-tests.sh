b85834e9dc96c7af89069772663cdfdfd62207a2
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2019-02-04 20:19:22 +0000

    Xdebug 2.7.0RC1 and PHP 7.3.2RC1 (#555)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.26 7.2.14 7.3.1"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.26 7.2.14 7.3.1 7.3.2RC1"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
