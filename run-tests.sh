376c2ee3b3a26c4d436fac9c0678e597acbb9662
Yoshinari Takaoka <mumumu@mumumu.org>
2020-03-17 20:24:54 +0900

    Add PHP 7.4.4, 7.3.16, 7.2.29

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.28 7.3.15 7.4.3"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.29 7.3.16 7.4.4"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
