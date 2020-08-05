8257cb6bca9369de1f9ae8ef73432d51d37aafb9
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2020-08-05 15:29:02 +0100

    Add PHP 7.2.33, 7.3.21, 7.4.9 (#625)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.32 7.3.20 7.4.8"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.33 7.3.21 7.4.9"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
