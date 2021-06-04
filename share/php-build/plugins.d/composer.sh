307dec9d448a3a1df98c39ba32ba846903c62036
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2021-06-04 09:17:08 +0100

    Composer 2.1.1

--- share/php-build/plugins.d/composer.sh
+++ share/php-build/plugins.d/composer.sh
@@ -17,7 +17,7 @@ download_composer() {
     local composer_url="$1"
 
     if [ -z "$composer_url" ]; then
-        composer_url="https://getcomposer.org/download/2.0.13/composer.phar"
+        composer_url="https://getcomposer.org/download/2.1.1/composer.phar"
     fi
 
     if [ ! -f "$PHP_BUILD_TMPDIR/packages/composer.phar" ]; then
