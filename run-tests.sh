6ebb503202120ad3b6dcbbaf6482350ea0f1065b
TAGAWA Takao <dounokouno@gmail.com>
2021-12-17 11:34:00 +0900

    Add 7.4.27

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.26 8.0.13 8.1.0"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.27 8.0.13 8.1.0"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
