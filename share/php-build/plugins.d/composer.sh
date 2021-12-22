cd551a88961ac766946e41c030a40a85effd77ff
Graham Campbell <hello@gjcampbell.co.uk>
2021-12-22 10:49:00 +0000

    Upgrade to Composer 2.2.0

--- share/php-build/plugins.d/composer.sh
+++ share/php-build/plugins.d/composer.sh
@@ -17,7 +17,7 @@ download_composer() {
     local composer_url="$1"
 
     if [ -z "$composer_url" ]; then
-        composer_url="https://getcomposer.org/download/2.1.9/composer.phar"
+        composer_url="https://getcomposer.org/download/2.2.0/composer.phar"
     fi
 
     if [ ! -f "$PHP_BUILD_TMPDIR/packages/composer.phar" ]; then
