89fcae88dcfab8ee21851f8eb9e29a58c96df1da
TAGAWA Takao <dounokouno@gmail.com>
2022-02-16 10:26:59 +0900

    Add 7.4.28, 8.0.16 and 8.1.3

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.27 8.0.15 8.1.2"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.28 8.0.16 8.1.3"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
