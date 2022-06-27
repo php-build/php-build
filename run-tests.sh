3b79b70e6087711b53355acb6aaaf4ebd58f8ca8
TAGAWA Takao <dounokouno@gmail.com>
2022-06-28 01:06:57 +0900

    Add 8.2.0alpha2

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.30 8.0.20 8.1.7 8.2.0alpha1"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.30 8.0.20 8.1.7 8.2.0alpha2"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
