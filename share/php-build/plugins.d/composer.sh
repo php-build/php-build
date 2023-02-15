f178cffd43b4a33660d650e5d042e7fcb076637c
Graham Campbell <GrahamCampbell@users.noreply.github.com>
2023-02-15 14:02:12 +0000

    Upgrade to Composer 2.2.21

--- share/php-build/plugins.d/composer.sh
+++ share/php-build/plugins.d/composer.sh
@@ -17,7 +17,7 @@ download_composer() {
     local composer_url="$1"
 
     if [ -z "$composer_url" ]; then
-        composer_url="https://getcomposer.org/download/2.2.18/composer.phar"
+        composer_url="https://getcomposer.org/download/2.2.21/composer.phar"
     fi
 
     if [ ! -f "$PHP_BUILD_TMPDIR/packages/composer.phar" ]; then
