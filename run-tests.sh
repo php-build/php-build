b97a71b57b64ffc7ec14e9c4d6207821d14f3f68
Koichi MATSUMOTO <mzch@me.com>
2023-02-15 10:00:54 +0900

    Add 8.0.28, 8.1.16, 8.2.3 (#745)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.27 8.1.14 8.2.1"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.28 8.1.16 8.2.3"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
