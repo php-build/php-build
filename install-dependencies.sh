7db067a2468739061ec2cb043ac0c748d8a3e6ba
TAGAWA Takao <dounokouno@gmail.com>
2024-09-09 17:23:19 +0900

    Remove CentOS 7 support

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -62,17 +62,7 @@ case $DISTRO in
 		;;
 	rhel)
 		$SUDO yum install -y yum-utils epel-release
-		if [[ "$VERSION_ID" =~ ^7 ]]; then
-			$SUDO yum-config-manager --enable PowerTools
-			# Install gcc v10 and enable it
-			$SUDO yum install -y centos-release-scl
-			$SUDO yum install -y devtoolset-10-gcc-c++
-			# Countermeasures for the message on the right: "MANPATH: unbound variable"
-			MANPATH=""
-			# Enable devtoolset-10 for gcc v10
-			source /opt/rh/devtoolset-10/enable
-			$SUDO yum install -y autoconf autoconf213
-		elif [[ "$VERSION_ID" =~ ^8 ]]; then
+		if [[ "$VERSION_ID" =~ ^8 ]]; then
 			$SUDO yum install -y dnf-plugins-core
 			$SUDO yum config-manager --set-enabled powertools
 			$SUDO yum install -y autoconf autoconf213
