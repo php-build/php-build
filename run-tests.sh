5f878ca064c23c1085aa6228dd3d5cd6002ead31
Koichi MATSUMOTO <mzch@me.com>
2025-05-06 10:04:35 +0900

    Updated xdebug & Added 8.1.31-8.1.32, 8.2.26-8.2.28, 8.3.14-8.3.20, and 8.4.1-8.4.6 (#785)
    * Update xdebug version
    
    Update xdebug version
    
    * Add 8.1.31, 8.2.26 and 8.3.14
    
    Add 8.1.31, 8.2.26 and 8.3.14
    
    * Add 8.4.1
    
    Add 8.4.1
    
    * exclude rocky from building 8.4
    
    exclude rocky from building 8.4
    
    * Updated xdebug version.
    
    * Add 8.2.27, 8.3.15 and 8.4.2
    
    * Add 8.3.16 & 8.4.3
    
    * Update xdebug version.
    
    * Add 8.3.17 & 8.4.4
    
    * Update ci.yml
    
    * Add 8.3.19 and 8.4.5
    
    * Add 8.1.32 and 8.2.28
    
    * Add 8.3.20 and 8.4.6
    
    * update .travis.yml
    
    * Fix #717.
    
    * fix typo.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.30 8.1.30 8.2.25 8.3.13"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.30 8.1.32 8.2.28 8.3.20 8.4.6"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
