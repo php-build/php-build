56105799393525912ccf192a28851a3307108357
Koichi MATSUMOTO <mzch@me.com>
2025-09-26 09:05:11 +0900

    Add 8.3.26 & 8.4.13.

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Update this when a new stable version comes around
-STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.30 8.1.33 8.2.29 8.3.25 8.4.12"
+STABLE_DEFINITIONS="5.3.29 5.4.45 5.5.38 5.6.40 7.0.33 7.1.33 7.2.34 7.3.33 7.4.33 8.0.30 8.1.33 8.2.29 8.3.26 8.4.13"
 
 TIME="$(date "+%Y%m%d%H%M%S")"
 
