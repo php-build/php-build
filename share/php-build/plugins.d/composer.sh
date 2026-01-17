620a2a42ab22587bcca57ef3ce7871af9c8d6bf0
Koichi MATSUMOTO <mzch@me.com>
2026-01-17 21:29:42 +0900

    fix an installation bug of composer.

--- share/php-build/plugins.d/composer.sh
+++ share/php-build/plugins.d/composer.sh
@@ -4,6 +4,8 @@ PHP="$PREFIX/bin/php"
 
 install_composer() {
 
+    log Composer "Downloading from $composer_url"
+
     download_composer
 
     log Composer "Installing executable in $PREFIX/bin/composer"
@@ -11,6 +13,8 @@ install_composer() {
     mv "$PREFIX/bin/composer.phar" "$PREFIX/bin/composer"
 
     chmod +x "$PREFIX/bin/composer"
+
+    log Composer "Downloaded composer."
 }
 
 download_composer() {
@@ -21,11 +25,13 @@ download_composer() {
         composer_url="https://getcomposer.org/download/latest-stable/composer.phar"
     fi
 
-    log Composer "Downloading from $composer_url"
+    if [ ! -d "$PREFIX/bin" ] ; then
+      mkdir -p "$PREFIX/bin"
+    fi
 
     if [ -f "$PREFIX/bin/composer.phar" ]; then
       rm "$PREFIX/bin/composer.phar"
     fi
 
-    curl -s $composer_url -o "$PREFIX/bin/composer.phar"
+    exec curl -sS $composer_url -o "$PREFIX/bin/composer.phar"
 }
