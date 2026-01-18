4b04705d79f678fbe26d39ceb4c1c81d4db75dd3
Koichi MATSUMOTO <mzch@me.com>
2026-01-18 15:15:30 +0900

    fix missing permission of composer.

--- share/php-build/plugins.d/composer.sh
+++ share/php-build/plugins.d/composer.sh
@@ -4,34 +4,21 @@ PHP="$PREFIX/bin/php"
 
 install_composer() {
 
-    log Composer "Downloading from $composer_url"
-
-    download_composer
-
-    log Composer "Installing executable in $PREFIX/bin/composer"
-
-    mv "$PREFIX/bin/composer.phar" "$PREFIX/bin/composer"
-
-    chmod +x "$PREFIX/bin/composer"
-
-    log Composer "Downloaded composer."
-}
-
-download_composer() {
-
     local composer_url="$1"
 
     if [ -z "$composer_url" ]; then
         composer_url="https://getcomposer.org/download/latest-stable/composer.phar"
     fi
 
+    log Composer "Downloading from $composer_url"
+
     if [ ! -d "$PREFIX/bin" ] ; then
-      mkdir -p "$PREFIX/bin"
+        mkdir -p "$PREFIX/bin"
     fi
 
-    if [ -f "$PREFIX/bin/composer.phar" ]; then
-      rm "$PREFIX/bin/composer.phar"
-    fi
+    curl -sS "$composer_url" -o "$PREFIX/bin/composer"
 
-    exec curl -sS $composer_url -o "$PREFIX/bin/composer.phar"
+    chmod +x "$PREFIX/bin/composer"
+
+    log Composer "Downloaded."
 }
