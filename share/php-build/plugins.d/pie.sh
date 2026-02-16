1b68480f970186b3c6c3d5bba58013ca8911ff18
Koichi MATSUMOTO <mzch@me.com>
2026-02-16 22:09:03 +0900

    Add pie.

---
+++ share/php-build/plugins.d/pie.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+install_pie() {
+
+    local pie="$1"
+    local pie_path="$PREFIX/bin/pie"
+
+    if [[ -z "$pie_url" ]]; then
+        pie_url="https://https://github.com/php/pie/release/latest/download/pie.phar"
+    fi
+
+    log Pie "Downloading from $pie_url"
+
+    if [[ ! -d "$PREFIX/bin" ]] ; then
+        mkdir -p "$PREFIX/bin"
+    fi
+    if [[ -f "$pie_path" ]] ; then
+        rm -f "$pie_path"
+    fi
+
+    curl -sS "$pie_url" -o "$pie_path"
+    if [[ $? -ne 0 ]] ; then
+      wget -q -O "$pie_path" "$pie_url"
+      if [[ $? -ne 0 ]] ; then
+        log Composer "You don't have curl nor wget."
+        exit 2
+      fi
+    fi
+
+    chmod +x "$pie_path"
+
+    log Pie "Downloaded."
+}
